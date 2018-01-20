858e5f3b9aabc9c02956695440149dda5af24a53
TeanLee <ltt598625763@163.com>
2018-01-20 11:21:39 +0800

    引入了axios引用数据

--- AwesomePOS/src/components/page/Pos.vue
+++ AwesomePOS/src/components/page/Pos.vue
@@ -29,14 +29,70 @@
                   </el-tab-pane>
               </el-tabs>
           </el-col>
-          <el-col :span='3'>
-              我是产品栏
+          <el-col :span='17'>
+              <div class="often-goods">
+                  <div class="title">常用商品</div>
+                  <div class="often-goods-list">
+                      <ul>
+                          <li v-for="good in oftenGoods" :key="good.id">
+                              <span>{{good.goodsName}}</span>
+                              <span class="o-price">￥{{good.price}}元</span>
+                          </li>
+                      </ul>
+                  </div>
+              </div>
+
+                <div class="goods-type">
+                    <el-tabs>
+                        <el-tab-pane label="汉堡">
+                            <div>
+                                <ul class='cookList'>
+                                    <li v-for="goods in type0Goods" :key="goods.id">
+                                        <span class="foodImg"><img :src="goods.goodsImg" width="100%"></span>
+                                        <span class="foodName">{{goods.goodsName}}</span>
+                                        <span class="foodPrice">￥{{goods.price}}.00元</span>
+                                    </li>
+                                </ul>
+                            </div>
+                        </el-tab-pane>
+                            <el-tab-pane label="小食">
+                                <ul class='cookList'>
+                                    <li v-for="goods in type1Goods" :key="goods.id">
+                                        <span class="foodImg"><img :src="goods.goodsImg" width="100%"></span>
+                                        <span class="foodName">{{goods.goodsName}}</span>
+                                        <span class="foodPrice">￥{{goods.price}}.00元</span>
+                                    </li>
+                                </ul>
+                            </el-tab-pane>
+                        <el-tab-pane label="饮料">
+                            <ul class='cookList'>
+                                <li v-for="goods in type2Goods" :key="goods.id">
+                                    <span class="foodImg"><img :src="goods.goodsImg" width="100%"></span>
+                                    <span class="foodName">{{goods.goodsName}}</span>
+                                    <span class="foodPrice">￥{{goods.price}}.00元</span>
+                                </li>
+                            </ul>
+                        </el-tab-pane>
+                        <el-tab-pane label="套餐">
+                            <ul class='cookList'>
+                                <li v-for="goods in type3Goods" :key="goods.id">
+                                    <span class="foodImg"><img :src="goods.goodsImg" width="100%"></span>
+                                    <span class="foodName">{{goods.goodsName}}</span>
+                                    <span class="foodPrice">￥{{goods.price}}.00元</span>
+                                </li>
+                            </ul>
+                        </el-tab-pane>
+                    </el-tabs>
+                </div>
+
           </el-col>
       </el-row>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
     name: "pos",
     data() {
@@ -60,13 +116,43 @@ export default {
             goodsName: '甜筒',
             price: 8,
             count:1
-            }]
+            }],
+            oftenGoods: [],
+            type0Goods:[],
+            type1Goods:[],
+            type2Goods:[],
+            type3Goods:[]
         }
     },
     mounted: function() {
         // 动态设置订单栏的高度为屏幕的高度  不能设置高度直接为100%
         var orderHeight = document.body.clientHeight;
         document.getElementById('order-list').style.height = orderHeight + 'px';
+    },
+    created: function() {
+        axios.get('http://jspang.com/DemoApi/oftenGoods.php')
+        .then(response => {
+            console.log(response);
+            this.oftenGoods = response.data;
+        })
+        .catch(error => {
+            console.log('error');
+            alert('error');
+        })
+        axios.get('http://jspang.com/DemoApi/typeGoods.php')
+        .then(response=>{
+            console.log(response);
+            //this.oftenGoods=response.data;
+            this.type0Goods=response.data[0];
+            this.type1Goods=response.data[1];
+            this.type2Goods=response.data[2];
+            this.type3Goods=response.data[3];
+    
+        })
+        .catch(error=>{
+            console.log(error);
+            alert('网络错误，不能访问');
+        })
     }
 }
 </script>
@@ -80,4 +166,55 @@ export default {
   .div-btn {
       margin-top: 10px;
   }
+  .title{
+       height: 20px;
+       border-bottom:1px solid #D3DCE6;
+       background-color: #F9FAFC;
+       padding:10px;
+   }
+   .often-goods-list ul li{
+      list-style: none;
+      float:left;
+      border:1px solid #E5E9F2;
+      padding:10px;
+      margin:5px;
+      background-color:#fff;
+   }
+  .o-price{
+      color:#58B7FF; 
+   }
+   .goods-type {
+       clear: both;
+   }
+   .cookList li{
+       list-style: none;
+       width:23%;
+       border:1px solid #E5E9F2;
+       height: auot;
+       overflow: hidden;
+       background-color:#fff;
+       padding: 2px;
+       float:left;
+       margin: 2px;
+ 
+   }
+   .cookList li span{
+       
+        display: block;
+        float:left;
+   }
+   .foodImg{
+       width: 40%;
+   }
+   .foodName{
+       font-size: 18px;
+       padding-left: 10px;
+       color:brown;
+ 
+   }
+   .foodPrice{
+       font-size: 16px;
+       padding-left: 10px;
+       padding-top:10px;
+   }
 </style>
